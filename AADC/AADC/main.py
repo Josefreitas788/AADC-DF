ddec095db0a7b8d615fd46ee0cc4cee7ffe36bfe
Heloise Katharine <katharineheloise@gmail.com>
2022-01-01 00:59:39 -0300

    Update

--- AADC/AADC/main.py
+++ AADC/AADC/main.py
@@ -4,10 +4,11 @@ from IPython.display import display
 
 # Importando os dados
 # add o caminho dos dados 
-dados = pd.read_csv('#', sep = ';')
+dados = pd.read_csv('D:\heloh\Documents\Faculdade\Covid_DF.csv', sep = ';')
+
 
 # Colunas Selecionadas
-colunas_selecionadas =  ['paciente_idade', 'paciente_enumsexobiologico', 'paciente_racacor_valor', 'paciente_endereco_nmmunicipio', 'paciente_endereco_nmpais', 'paciente_endereco_uf', 'paciente_nacionalidade_enumnacionalidade', 'estalecimento_nofantasia', 'vacina_grupoatendimento_nome', 'vacina_categoria_nome', 'vacina_descricao_dose', 'vacina_nome' ]
+colunas_selecionadas =  ['paciente_idade', 'paciente_enumsexobiologico', 'paciente_racacor_valor', 'paciente_endereco_nmmunicipio', 'paciente_endereco_nmpais', 'paciente_endereco_uf', 'paciente_nacionalidade_enumnacionalidade', 'estalecimento_nofantasia', 'vacina_grupoatendimento_nome', 'vacina_categoria_nome', 'vacina_descricao_dose', 'vacina_nome', 'paciente_endereco_cep']
 
 # Excluir = paciente_nacionalidade_enumnacionalidade 
 
@@ -28,23 +29,47 @@ print(dados_vacina.isnull().sum())
 #print(dados_vacina.loc[dados_vacina['paciente_racacor_valor'] == 50.59404737212269] )
 dados_vacina.drop(dados_vacina.loc[dados_vacina['paciente_racacor_valor'] == 50.59404737212269].index, inplace=True)
 
+#Alteração na UF do paciente
+dados_vacina.loc[dados_vacina['paciente_endereco_uf'] == 'XX'] = 'Não informado'
+
 # Exportando dados
 #dados_vacina.to_csv('dados_vacina.csv')
 
 
 # Quantidade de pessoas que tomaram a 1°, 2° e 3° dose
-def quant_dose123():
+def graf_quant_dose123():
 
-    coluna_descricao_dose = dados_vacina['vacina_descricao_dose']
-    
-    doses = coluna_descricao_dose.value_counts()
+    # Filtrando dados do DataFrame
+    colunas = ['vacina_descricao_dose']
+    doses = dados_vacina.filter(items=colunas)
+ 
+    graf = doses.value_counts()
+
     labels = ['1° Dose', '2° Dose', 'Dose única']
     plt.style.use("ggplot")
     explode = (0.1, 0.0, 0.0)
     
-    doses.plot.pie(autopct='%1.1f%%', explode = explode, shadow=True, startangle = 90, ylabel='', title = 'Porcentagem de pessoas que tomaram a 1° dose, 2° dose e a dose única.\n', subplots=True, labels = ['', '', '']) 
+    graf.plot.pie(autopct='%1.1f%%', explode = explode, shadow=True, startangle = 90, ylabel='', title = 'Porcentagem de pessoas que tomaram a 1° dose, 2° dose e a dose única.\n', subplots=True, labels = ['', '', '']) 
 
     L = plt.legend( bbox_to_anchor=(1, 0, 0.5, 1), loc='center left', labels = labels)
     plt.show() 
-    
-quant_dose123()
+
+
+# Classificação por região geográfica.
+def graf_regiao_geografica ():
+
+    # Filtrando dados do DataFrame
+    colunas = ['paciente_endereco_cep', 'paciente_endereco_nmmunicipio', 'paciente_endereco_uf']
+    dados_regiao_geografica = dados_vacina.filter(items=colunas)
+
+    # Gráfico UF do paciente | falta terminar
+    #graf = dados_vacina['paciente_endereco_uf'].value_counts()
+    graf = dados_vacina.groupby('paciente_endereco_uf').count()
+    #print(graf)
+    graf.plot.barh()
+    plt.show() 
+
+
+# Chamando as funções 
+#graf_quant_dose123()
+graf_regiao_geografica()
