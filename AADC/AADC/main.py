754e7ee3c668418cfe4ca087d9f68f8fe65971c4
Heloise Katharine <katharineheloise@gmail.com>
2022-01-09 22:24:37 -0300

    Update

--- AADC/AADC/main.py
+++ AADC/AADC/main.py
@@ -69,40 +69,62 @@ def graf_regiao_geografica_estados():
 
 def graf_regiao_geografica_paises():
 
-    paises  = ['Brasil', 'Não informado', 'Ruanda' 'Venezuela' ,'Japão', 'Congo', 'Bolívia' , 'Portugal', 'São Tomé e Príncipe', 'Gibraltar', 'Colômbia', 'Andorra']
-
-    labels1 = 'Ruanda', 'Venezuela', 'Bolívia' ,  'Congo', 'Japão', 'Andorra','Colômbia', 'Gibraltar', 'Portugal', 'São Tomé e Príncipe'
-    labels0 = 'Brasil', 'Outros'
-
+    label = 'Ruanda', 'Venezuela', 'Bolívia' ,  'Congo', 'Japão', 'Andorra','Colômbia', 'Gibraltar', 'Portugal', 'São Tomé e Príncipe'
 
     # Filtrando dados do DataFrame
     colunas = ['paciente_endereco_nmpais']
     regiao_geografica_paises = dados_vacina.filter(items=colunas)
 
     # Gráfico país do paciente 
-    #graf_paises = (regiao_geografica_paises['paciente_endereco_nmpais'].value_counts())
+    graf_paises = (regiao_geografica_paises['paciente_endereco_nmpais'].value_counts())
 
-    # mudar as labels
-    #graf_paises = (regiao_geografica_paises[(regiao_geografica_paises['paciente_endereco_nmpais'] != 'BRASIL') = 'D'] .value_counts())
-
+    # Países estrangeiros 
     graf_paises_estrangeiros = regiao_geografica_paises[(regiao_geografica_paises['paciente_endereco_nmpais'] != 'BRASIL') & (regiao_geografica_paises['paciente_endereco_nmpais'] != 'Não informado')].value_counts()
 
-    #####
-    '''
+    # Criação do gráfico
     fig, axs = plt.subplots(1,2)
+    axs[0].set_title('Países')
     axs[0].pie(graf_paises, shadow=True, startangle=90)
-    axs[1].pie(graf_paises_estrangeiros, labels=labels1, shadow=True, startangle=90)
-    '''
-    #####
-    
-    graf_paises.plot.pie(ylabel='', shadow=True, startangle=90)
-
+    axs[1].set_title('Países estrangeiros')
+    axs[1].pie(graf_paises_estrangeiros, labels=label, shadow=True, startangle=90)
     plt.show() 
 
+# Faixa etária das pessoas que tomaram a vacina.
+
+def faixa_etaria():
+
+  # Filtrando dados do DataFrame
+    colunas = ['paciente_idade']
+    idade = dados_vacina.filter(items=colunas)
+    
+    #print(graf)
+    
+    # Criação do gráfico
+    import re
+
+    fig, ax = plt.subplots() 
+    graf = idade.value_counts()
+
+    graf.plot.bar(ax=ax)
+   
+    #ax.bar(graf)
+    #graf.plot.bar()
+    ax.set_title('Blá blá blá')
+
+    handles, labels = ax.get_legend_handles_labels()
+    print(labels)
+    new_labels = [re.search('\((.+?)\.', label).group(1) for label in labels]
+    print(new_labels)
+    ax.legend(new_labels)
+
+    plt.show()
+    
 
 
 
 
+
+###########################################################################################
 
 # Testes
 def graf_regiao_geografica_df ():
@@ -117,6 +139,7 @@ def graf_regiao_geografica_df ():
 
     print(dados_regiao_geografica_df)
 
+# Conexão com o banco de dados 
 def exportar_dados():
     from pymongo import MongoClient
 
@@ -139,5 +162,6 @@ def exportar_dados():
 #graf_quant_dose123()
 #graf_regiao_geografica_estados()
 #graf_regiao_geografica_df()
-graf_regiao_geografica_paises()
+#graf_regiao_geografica_paises()
 #exportar_dados()
+faixa_etaria()
