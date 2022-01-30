ea6cca3c0043c853ebc28908d8f9345318686516
Leandro Marques <leandromqrs@hotmail.com>
2022-01-30 14:08:40 -0300

    Corrige bugs visuais dos gráficos

--- analysis/graph.py
+++ analysis/graph.py
@@ -18,34 +18,25 @@ if not os.path.isfile(arquivo_csv):
 
 dados = pd.read_csv(arquivo_csv, sep = ';')
 
-# Colunas Selecionadas
-colunas_selecionadas =  ['paciente_idade', 'paciente_enumsexobiologico', 'paciente_racacor_valor', 'paciente_endereco_nmmunicipio', 'paciente_endereco_nmpais', 'paciente_endereco_uf', 'estalecimento_nofantasia', 'vacina_grupoatendimento_nome', 'vacina_categoria_nome', 'vacina_descricao_dose', 'vacina_nome', 'paciente_endereco_cep']
+#colunas_selecionadas =  ['paciente_idade', 'paciente_enumsexobiologico', 'paciente_racacor_valor', 'paciente_endereco_nmmunicipio', 'paciente_endereco_nmpais', 'paciente_endereco_uf', 'estalecimento_nofantasia', 'vacina_grupoatendimento_nome', 'vacina_categoria_nome', 'vacina_descricao_dose', 'vacina_nome', 'paciente_endereco_cep']
 
 # Novo Dataframe com as colunas selecionadas
-dados_vacina = dados.filter(items = colunas_selecionadas)
+#dados_vacina = dados.filter(items = colunas_selecionadas)
 
 # Idades inconsistentes foram substituidas pela média aritmética da coluna 'paciente_idade'
-dados_vacina.loc[dados_vacina['paciente_idade'].isnull()] = dados_vacina['paciente_idade'].mean()
+#dados_vacina.loc[dados_vacina['paciente_idade'].isnull()] = dados_vacina['paciente_idade'].mean()
 
 # Dados nulos das outras colunas foram substituidos por 'Não informado'
-dados_vacina.fillna("Não informado", inplace = True)
+#dados_vacina.fillna("Não informado", inplace = True)
 
 # Soma de valores nulos em cada coluna
 #print(dados_vacina.isnull().sum())
 
 # Drop paciente_racacor_valor = 50.59404737212269]
-dados_vacina.drop(dados_vacina.loc[dados_vacina['paciente_racacor_valor'] == 50.59404737212269].index, inplace=True)
+#dados_vacina.drop(dados_vacina.loc[dados_vacina['paciente_racacor_valor'] == 50.59404737212269].index, inplace=True)
 
 #Alteração na UF do paciente
-dados_vacina.loc[dados_vacina['paciente_endereco_uf'] == 'XX'] = 'Não informado'
-
-####################### Função para salvar os graficos #######################
-
-def save_graph(name, buf):
-    if not os.path.isfile(name):
-        f = open(f"aadc/static/img/{name}.png", "wb")
-        f.write(buf)
-        f.close()
+#dados_vacina.loc[dados_vacina['paciente_endereco_uf'] == 'XX'] = 'Não informado'
 
 ####################### Quantidade de pessoas que tomaram a 1°, 2° e 3° dose #######################
 
@@ -53,7 +44,7 @@ def graf_quant_dose123(name):
     # Filtrando dados do DataFrame
 
     colunas = ['vacina_descricao_dose']
-    vacina_dose = dados_vacina.filter(items=colunas)
+    vacina_dose = dados.filter(items=colunas)
     
     # Removendo dados de não informado
     vacina_dose.drop(vacina_dose.loc[vacina_dose['vacina_descricao_dose'] == 'Não informado'].index, inplace=True)
@@ -61,23 +52,16 @@ def graf_quant_dose123(name):
     graf = vacina_dose.value_counts()
 
     labels = ['1° Dose', '2° Dose', 'Dose única']
-    plt.style.use("ggplot")
+    #plt.style.use("ggplot")
     explode = (0.1, 0.0, 0.0)
+    graf.plot.pie(figsize=(7, 5), autopct='%1.1f%%', shadow=True, startangle = 90, ylabel='', title = 'Porcentagem de pessoas que tomaram a 1° dose, 2° dose e a dose única.\n', subplots=True, labels = labels, explode=explode) 
 
-    labels1 = ['', '', '', '']
-    graf.plot.pie(autopct='%1.1f%%', shadow=True, startangle = 90, ylabel='', title = 'Porcentagem de pessoas que tomaram a 1° dose, 2° dose e a dose única.\n', subplots=True, labels = labels1,explode= explode ) 
-
-    L = plt.legend( bbox_to_anchor=(1, 0, 0.5, 1), loc='center left', labels = labels)
+    #plt.legend( bbox_to_anchor=(1, 0, 0.5, 1), loc='center left', labels = labels)
     #plt.show() 
-
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
 
 #######################Classificação por região geográfica.(estados do Brasil)#######################
 
@@ -85,22 +69,17 @@ def graf_regiao_geografica_estados(name):
 
     # Filtrando dados do DataFrame
     colunas = ['paciente_endereco_uf']
-    dados_regiao_geografica = dados_vacina.filter(items=colunas)
+    dados_regiao_geografica = dados.filter(items=colunas)
 
     # Gráfico UF do paciente 
-    graf = (dados_regiao_geografica['paciente_endereco_uf'].value_counts())
+    graf = dados_regiao_geografica['paciente_endereco_uf'].value_counts()
 
-    graf.plot.bar(title = 'Localização geográfica das pessoas que se vacinaram no DF\n', xlabel= 'Estados', ylabel = 'Quantidade de pessoas')
+    graf.plot.bar(figsize=(7, 5), title = 'Localização geográfica das pessoas que se vacinaram no DF\n', xlabel= 'Estados', ylabel = 'Quantidade de pessoas')
     #plt.show() 
-
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
 
 
 ####################### Classificação por região geográfica. (paises) #######################
@@ -111,7 +90,7 @@ def graf_regiao_geografica_paises(name):
 
     # Filtrando dados do DataFrame
     colunas = ['paciente_endereco_nmpais']
-    regiao_geografica_paises = dados_vacina.filter(items=colunas)
+    regiao_geografica_paises = dados.filter(items=colunas)
 
     # Gráfico país do paciente 
     graf_paises = (regiao_geografica_paises['paciente_endereco_nmpais'].value_counts())
@@ -127,14 +106,10 @@ def graf_regiao_geografica_paises(name):
     axs[1].pie(graf_paises_estrangeiros, labels=label, shadow=True, startangle=90)
   
     #Django
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
 
 ####################### Faixa etária das pessoas que tomaram a vacina. #######################
 
@@ -142,7 +117,7 @@ def faixa_etaria(name):
 
   # Filtrando dados do DataFrame
     colunas = ['paciente_idade']
-    idade = dados_vacina.filter(items=colunas)
+    idade = dados.filter(items=colunas)
     #idade = dados_vacina['paciente_idade']
  
     # Alteração na idade  do paciente
@@ -150,23 +125,19 @@ def faixa_etaria(name):
     
     # Criação do gráfico
     
-    idade.plot.hist(bins=30,color= 'green') 
+    idade.plot.hist(bins=30, figsize=(7, 5), color= 'green') 
 
     #Django
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri    
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
 
 ####################### Classificação por vacina (Pfizer..) #######################
 def name_vacina(name):
     # Filtrando dados do DataFrame
     colunas = ['vacina_nome']
-    vacinas = dados_vacina['vacina_nome']
+    vacinas = dados.filter(items=colunas)
 
     vacinas = vacinas.replace('Vacina Covid-19 - Covishield', 'Covishield')
     vacinas = vacinas.replace('Covid-19-Coronavac-Sinovac/Butantan', 'Sinovac/Butantan')
@@ -176,20 +147,14 @@ def name_vacina(name):
 
     # Gráfico
     graf = vacinas.value_counts()
-    graf.plot.bar(title='Vacinas utilizadas x Quantidade')
+    graf.plot.bar(title='Vacinas utilizadas x Quantidade', figsize=(7, 5),)
     #print(graf)
 
     #Django
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri
-
-
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
 
 ####################### Classificação por Etnia #######################
 def vacina_etnia(name):
@@ -198,83 +163,67 @@ def vacina_etnia(name):
     dados.drop(dados_limpos , inplace=True)
     graf = (dados['paciente_racacor_valor'].value_counts())
     #Criando o grafico
-    graf.plot.bar(title = 'Etnia\n',xlabel= 'Vacinas', ylabel = 'Frequência', color= "ORANGE")
+    graf.plot.bar(title = 'Etnia\n',xlabel= 'Vacinas', ylabel = 'Frequência', color= "ORANGE", figsize=(7, 5),)
 
     #Django
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri    
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
+   
 ####################### Classificação por Grupo de vacinados #######################
 
 def vacina_categoria(name):
     #Filtrando as colunas
     graf = (dados['vacina_categoria_nome'].value_counts())
     #Criando o grafico
-    graf.plot.bar(title = 'Categorias mais vacinadas no DF\n', xlabel= 'Vacinas', ylabel = 'Frequência', color = "green")
+    graf.plot.bar(figsize=(7, 5), title = 'Categorias mais vacinadas no DF\n', xlabel= 'Vacinas', ylabel = 'Frequência', color = "green")
 
     #Django
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri    
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
+
 ####################### Classificação por genero biologico #######################
 
 def vacina_genero_biologico(name):
     #Filtrando coluna
     graf = (dados['paciente_enumsexobiologico'].value_counts())
     #Criando o gráfico
-    graf.plot.pie(title = 'Gênero que mais se vacinou no  DF\n', autopct='%1.1f%%',xlabel= 'Vacinas', ylabel = 'Porcentagem')
+    graf.plot.pie(figsize=(7, 5), title = 'Gênero que mais se vacinou no  DF\n', autopct='%1.1f%%',xlabel= 'Vacinas', ylabel = 'Porcentagem')
 
     #Django
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri    
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
+
 ####################### Classificação por UF dos vacinados #######################
 def uf_dos_vacinados(name):
     #Filtrando coluna
     graf = (dados['paciente_endereco_uf'].value_counts())
     #Criando o gráfico
-    graf.plot.bar(title = 'UF dos pacientes que foram vacinados no DF\n', xlabel= 'Vacinas', ylabel = 'Frequência', color = "Yellow")
+    graf.plot.bar(figsize=(7, 5), title = 'UF dos pacientes que foram vacinados no DF\n', xlabel= 'Vacinas', ylabel = 'Frequência', color = "Yellow")
 
     #Django
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri    
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
+  
 ####################### Classificação das doses ja tomadas #######################
 def dose_tomada(name):
     #Filtrando as colunas
     graf = (dados['vacina_descricao_dose'].value_counts())
     #Criando os gráficos
-    graf.plot.barh(title = 'Quantidade das doses já tomadas DF\n', xlabel= 'Vacinas', ylabel = 'Frequência', color="purple")
+    graf.plot.barh(figsize=(7, 5), title = 'Quantidade das doses já tomadas DF\n', xlabel= 'Vacinas', ylabel = 'Frequência', color="purple")
 
     #Django
+    plt.tight_layout()
     fig = plt.gcf()
-    buf = io.BytesIO()
-    fig.savefig(buf, format = 'png')
-    buf.seek(0)
-    save_graph(name, buf.read())
-    #string = base64.b64encode(buf.read())
-    #uri = urllib.parse.quote(string)
-    #return uri  
+    fig.savefig(f"aadc/static/img/{name}.png", format = 'png')
+    plt.close(fig)
 
 # Chamando as funções 
 Quantidade_doses = Graphic(imageGraphic = graf_quant_dose123("graf_quant_dose"))
